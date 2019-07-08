8e96427130c010003794fa86c504d58d89715c75
luozijian <luozijian@zuzuche.com>
2019-07-08 23:29:22 +0800

    eureka集群

--- src/main/java/com/luo/App.java
+++ src/main/java/com/luo/App.java
@@ -2,6 +2,7 @@ package com.luo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableEurekaClient
 public class App 
 {
     public static void main( String[] args )
